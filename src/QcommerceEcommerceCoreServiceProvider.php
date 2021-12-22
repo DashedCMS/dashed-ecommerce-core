31ac6d7edd44b3c543de6bff7f1f5695d12b4bc2
Robin van Maasakker <robin@qubiqx.com>
2021-12-22 18:42:31 +0100

    Update

--- src/QcommerceEcommerceCoreServiceProvider.php
+++ src/QcommerceEcommerceCoreServiceProvider.php
@@ -2,24 +2,83 @@
 
 namespace Qubiqx\QcommerceEcommerceCore;
 
+use Filament\PluginServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
 use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Qubiqx\QcommerceEcommerceCore\Commands\QcommerceEcommerceCoreCommand;
 
-class QcommerceEcommerceCoreServiceProvider extends PackageServiceProvider
+class QcommerceEcommerceCoreServiceProvider extends PluginServiceProvider
 {
+    public static string $name = 'qcommerce-core';
+
+    public function bootingPackage()
+    {
+        $this->app->booted(function () {
+            $schedule = app(Schedule::class);
+//            $schedule->command(CreateSitemap::class)->daily();
+        });
+    }
+
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        cms()->builder('routeModels', [
+//            'page' => [
+//                'name' => 'Pagina',
+//                'pluralName' => 'Pagina\'s',
+//                'class' => Page::class,
+//                'nameField' => 'name',
+//                'routeHandler' => PageRouteHandler::class,
+//            ],
+        ]);
+
+        cms()->builder('settingPages', [
+//            'general' => [
+//                'name' => 'Algemeen',
+//                'description' => 'Algemene informatie van de website',
+//                'icon' => 'cog',
+//                'page' => GeneralSettingsPage::class,
+//            ],
+        ]);
+
         $package
-            ->name('qcommerce-ecommerce-core')
-            ->hasConfigFile()
+            ->name('qcommerce-core')
+            ->hasConfigFile([
+                'filament',
+                'filament-spatie-laravel-translatable-plugin',
+                'filesystems',
+                'laravellocalization',
+                'media-library',
+                'qcommerce-core',
+            ])
+            ->hasRoutes([
+                'frontend',
+            ])
             ->hasViews()
-            ->hasMigration('create_qcommerce-ecommerce-core_table')
-            ->hasCommand(QcommerceEcommerceCoreCommand::class);
+            ->hasAssets()
+            ->hasCommands([
+//                CreateSitemap::class,
+            ]);
+    }
+
+    protected function getStyles(): array
+    {
+        return [
+            'qcommerce-ecommerce-core' => str_replace('/vendor/qubiqx/qcommerce-ecommerce-core/src', '', str_replace('/packages/qubiqx/qcommerce-ecommerce-core/src', '', __DIR__)) . '/vendor/qubiqx/qcommerce-ecommerce-core/resources/dist/css/qcommerce-ecommerce-core.css',
+        ];
+    }
+
+    protected function getPages(): array
+    {
+        return array_merge(parent::getPages(), [
+//            SettingsPage::class,
+        ]);
+    }
+
+    protected function getResources(): array
+    {
+        return [
+//            PageResource::class,
+        ];
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require("laravel-mix");
 
-mix.postCss('./resources/assets/css/qcommerce-core.css', './resources/dist/css', [
+mix.postCss('./resources/assets/css/qcommerce-ecommerce-core.css', './resources/dist/css', [
     require('tailwindcss', './tailwind.config.js'),
 ])
