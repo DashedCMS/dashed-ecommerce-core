f54676c9f5a6ae293d604ad429ec8635f5daeee4
Robin van Maasakker <robin@qubiqx.com>
2022-01-01 13:04:56 +0100

    Update

--- src/Classes/ProductCategories.php
+++ src/Classes/ProductCategories.php
@@ -2,10 +2,23 @@
 
 namespace Qubiqx\QcommerceEcommerceCore\Classes;
 
+use Illuminate\Support\Facades\Cache;
 use Qubiqx\QcommerceEcommerceCore\Models\ProductCategory;
 
 class ProductCategories
 {
+    public static function get($limit = 4, $orderBy = 'created_at', $order = 'DESC')
+    {
+        return ProductCategory::with(['products'])->limit($limit)->orderBy($orderBy, $order)->get();
+    }
+
+    public static function getTopLevel($limit = 4, $orderBy = 'created_at', $order = 'DESC')
+    {
+        return Cache::tags(['product-categories'])->rememberForever("product-categories-top-level-$limit-$orderBy-$order", function () use ($limit, $orderBy, $order) {
+            return ProductCategory::with(['products'])->where('parent_category_id', null)->limit($limit)->orderBy($orderBy, $order)->get();
+        });
+    }
+
     public static function getFromIdsWithParents($selectedProductCategoriesIds)
     {
         $selectedProductCategories = ProductCategory::find($selectedProductCategoriesIds);
