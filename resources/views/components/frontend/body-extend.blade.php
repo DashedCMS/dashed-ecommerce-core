42e7907dd35e55235ddb0dbf8377461cadacb40b
Robin van Maasakker <robin@qubiqx.com>
2022-01-05 14:46:33 +0100

    Update

---
+++ resources/views/components/frontend/body-extend.blade.php
@@ -0,0 +1,16 @@
+@if(isset($product))
+    <x-qcommerce::frontend.products.schema :product="$product"></x-qcommerce::frontend.products.schema>
+@endif
+@if(isset($products))
+    @foreach($products as $product)
+        <x-qcommerce::frontend.products.schema :product="$product"></x-qcommerce::frontend.products.schema>
+    @endforeach
+@endif
+
+@if(env('APP_ENV') != 'local')
+    @if(isset($order) && $order->isPaidFor() && (Customsetting::get('facebook_pixel_conversion_id') || Customsetting::get('facebook_pixel_store_id')))
+        <script>
+            fbq('track', 'Purchase', {currency: "EUR", value: {{number_format($order->total, 2, '.', '')}}});
+        </script>
+    @endif
+@endif
