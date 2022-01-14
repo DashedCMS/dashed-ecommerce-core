8cfd12c4fd42b6f9be3d04ae94482d43ba5160e5
Robin van Maasakker <robin@qubiqx.com>
2022-01-14 07:25:15 +0100

    Update

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -339,7 +339,7 @@ class Product extends Model
 
     public function getUrl($locale = null)
     {
-        if (! $locale) {
+        if (!$locale) {
             $locale = App::getLocale();
         }
 
@@ -396,7 +396,7 @@ class Product extends Model
 
     public function getStatusAttribute()
     {
-        if (! $this->public) {
+        if (!$this->public) {
             return false;
         }
 
@@ -405,7 +405,7 @@ class Product extends Model
         }
 
         $active = false;
-        if (! $this->start_date && ! $this->end_date) {
+        if (!$this->start_date && !$this->end_date) {
             $active = true;
         } else {
             if ($this->start_date && $this->end_date) {
@@ -425,7 +425,7 @@ class Product extends Model
             }
         }
         if ($active) {
-            if (! $this->sku || ! $this->price) {
+            if (!$this->sku || !$this->price) {
                 $active = false;
             }
         }
@@ -491,7 +491,7 @@ class Product extends Model
                 }
 
                 //If something does not work correct, check if below code makes sure there is a active one
-                if (count($activeFilterOptionIds) && (! array_key_exists($activeFilterId, $filterOptionValues) || $this->id == $childProduct->id)) {
+                if (count($activeFilterOptionIds) && (!array_key_exists($activeFilterId, $filterOptionValues) || $this->id == $childProduct->id)) {
                     $filterOptionValues[$activeFilterId] = [
                         'id' => $activeFilter->id,
                         'name' => $filterName,
@@ -528,12 +528,12 @@ class Product extends Model
 
         foreach ($showableFilters as &$showableFilter) {
             foreach ($showableFilter['values'] as &$showableFilterValue) {
-                if (! $showableFilterValue['url']) {
+                if (!$showableFilterValue['url']) {
                     foreach ($childProducts as $childProduct) {
                         if ($childProduct->id != $this->id) {
                             $productIsCorrectForFilter = true;
                             foreach ($showableFilterValue['activeFilterOptionIds'] as $activeFilterOptionId) {
-                                if (! $childProduct->productFilters()->where('product_filter_option_id', $activeFilterOptionId)->exists()) {
+                                if (!$childProduct->productFilters()->where('product_filter_option_id', $activeFilterOptionId)->exists()) {
                                     $productIsCorrectForFilter = false;
                                 }
                             }
@@ -542,11 +542,11 @@ class Product extends Model
                                     if ($activeFilterValue['id'] != $showableFilterValue['id']) {
                                         $productHasCorrectFilterOption = true;
                                         foreach ($activeFilterValue['activeFilterOptionIds'] as $activeFilterOptionId) {
-                                            if (! $childProduct->productFilters()->where('product_filter_option_id', $activeFilterOptionId)->exists()) {
+                                            if (!$childProduct->productFilters()->where('product_filter_option_id', $activeFilterOptionId)->exists()) {
                                                 $productHasCorrectFilterOption = false;
                                             }
                                         }
-                                        if (! $productHasCorrectFilterOption) {
+                                        if (!$productHasCorrectFilterOption) {
                                             $productIsCorrectForFilter = false;
                                         }
                                     }
@@ -651,17 +651,17 @@ class Product extends Model
     {
         //Todo: make editable if expectedInStockDateValid should be checked or not
 
-        if (! $this->use_stock) {
+        if (!$this->use_stock) {
             if ($this->stock_status == 'out_of_stock') {
                 return false;
             }
         }
 
-        if (! $this->out_of_stock_sellable) {
+        if (!$this->out_of_stock_sellable) {
             return false;
         }
 
-        if (Customsetting::get('product_out_of_stock_sellable_date_should_be_valid', Sites::getActive(), 1) && ! $this->expectedInStockDateValid()) {
+        if (Customsetting::get('product_out_of_stock_sellable_date_should_be_valid', Sites::getActive(), 1) && !$this->expectedInStockDateValid()) {
             return false;
         }
 
@@ -670,7 +670,7 @@ class Product extends Model
 
     public function isPreorderable()
     {
-        return $this->inStock() && ! $this->hasDirectSellableStock() && $this->use_stock;
+        return $this->inStock() && !$this->hasDirectSellableStock() && $this->use_stock;
     }
 
     public function expectedInStockDate()
@@ -686,7 +686,7 @@ class Product extends Model
     public function expectedInStockDateInWeeks()
     {
         $expectedInStockDate = self::expectedInStockDate();
-        if (! $expectedInStockDate || Carbon::parse($expectedInStockDate) < now()) {
+        if (!$expectedInStockDate || Carbon::parse($expectedInStockDate) < now()) {
             return 0;
         }
 
@@ -777,44 +777,46 @@ class Product extends Model
 
     public function showableCharacteristics($withoutIds = [])
     {
-        $characteristics = [];
+        return Cache::tags(["product-$this->id"])->rememberForever("product-showable-characteristics-" . $this->id, function () {
+            $characteristics = [];
 
-        $parentProduct = $this->parentProduct;
+            $parentProduct = $this->parentProduct;
 
-        if ($parentProduct) {
-            $activeFilters = $parentProduct->activeProductFilters;
-        } else {
-            $activeFilters = $this->activeProductFilters;
-        }
-
-        foreach ($activeFilters as $activeFilter) {
-            $value = '';
-            foreach ($activeFilter->productFilterOptions as $option) {
-                if ($this->productFilters()->where('product_filter_option_id', $option->id)->exists()) {
-                    if ($value) {
-                        $value .= ', ';
-                    }
-                    $value .= $option->name;
-                }
+            if ($parentProduct) {
+                $activeFilters = $parentProduct->activeProductFilters;
+            } else {
+                $activeFilters = $this->activeProductFilters;
             }
-            $characteristics[] = [
-                'name' => $activeFilter->name,
-                'value' => $value,
-            ];
-        }
 
-        $allProductCharacteristics = ProductCharacteristics::orderBy('order')->get();
-        foreach ($allProductCharacteristics as $productCharacteristic) {
-            $thisProductCharacteristic = $this->productCharacteristics()->where('product_characteristic_id', $productCharacteristic->id)->first();
-            if ($thisProductCharacteristic && $thisProductCharacteristic->value && ! $productCharacteristic->hide_from_public && ! in_array($productCharacteristic->id, $withoutIds)) {
+            foreach ($activeFilters as $activeFilter) {
+                $value = '';
+                foreach ($activeFilter->productFilterOptions as $option) {
+                    if ($this->productFilters()->where('product_filter_option_id', $option->id)->exists()) {
+                        if ($value) {
+                            $value .= ', ';
+                        }
+                        $value .= $option->name;
+                    }
+                }
                 $characteristics[] = [
-                    'name' => $productCharacteristic->name,
-                    'value' => $thisProductCharacteristic->value,
+                    'name' => $activeFilter->name,
+                    'value' => $value,
                 ];
             }
-        }
 
-        return $characteristics;
+            $allProductCharacteristics = ProductCharacteristics::orderBy('order')->get();
+            foreach ($allProductCharacteristics as $productCharacteristic) {
+                $thisProductCharacteristic = $this->productCharacteristics()->where('product_characteristic_id', $productCharacteristic->id)->first();
+                if ($thisProductCharacteristic && $thisProductCharacteristic->value && !$productCharacteristic->hide_from_public && !in_array($productCharacteristic->id, $withoutIds)) {
+                    $characteristics[] = [
+                        'name' => $productCharacteristic->name,
+                        'value' => $thisProductCharacteristic->value,
+                    ];
+                }
+            }
+
+            return $characteristics;
+        });
     }
 
     public function getCharacteristicById($id)
