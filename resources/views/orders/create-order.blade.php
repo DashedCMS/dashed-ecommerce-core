d086a0546f48af5c696a82587f6ce6fc0b3d2361
Robin van Maasakker <robin@qubiqx.com>
2022-02-03 08:18:32 +0100

    Update

--- resources/views/orders/create-order.blade.php
+++ resources/views/orders/create-order.blade.php
@@ -6,10 +6,15 @@
         @if($loading)
             <p>loading</p>
         @else
-            <button
-                class="flex items-center gap-3 px-3 py-2 rounded-lg font-medium transition bg-primary-500 text-white mt-4">
-                Bestelling aanmaken
-            </button>
+            <x-filament::button type="submit"
+                                class="flex items-center gap-3 px-3 py-2 rounded-lg font-medium transition bg-primary-500 text-white mt-4">
+                <svg wire:loading wire:target="submit"
+                     class="w-6 h-6 mr-1 -ml-2 rtl:ml-1 rtl:-mr-2 filament-button-icon animate-spin"
+                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z"></path>
+                </svg>
+                <span>Bestelling aanmaken</span>
+            </x-filament::button>
         @endif
     </form>
 
