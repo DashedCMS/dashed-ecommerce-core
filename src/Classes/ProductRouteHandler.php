b5ddd701015c775d812c1dd495f133b4df50cfd2
Robinvm <Robinvm@users.noreply.github.com>
2022-03-25 11:18:11 +0000

    Fix styling

--- src/Classes/ProductRouteHandler.php
+++ src/Classes/ProductRouteHandler.php
@@ -17,14 +17,14 @@ class ProductRouteHandler
 
         if ($slugComponents[0] == Translation::get('products-slug', 'slug', 'products') && count($slugComponents) == 2) {
             $product = Product::thisSite();
-            if (!auth()->check() || auth()->user()->role != 'admin') {
+            if (! auth()->check() || auth()->user()->role != 'admin') {
                 $product->publicShowable();
             }
             $product->where('slug->' . App::getLocale(), $slugComponents[1])->first();
 
-            if (!$product) {
+            if (! $product) {
                 foreach (Product::thisSite()->publicShowable()->get() as $possibleProduct) {
-                    if (!$product && $possibleProduct->slug == $slugComponents[1]) {
+                    if (! $product && $possibleProduct->slug == $slugComponents[1]) {
                         $product = $possibleProduct;
                     }
                 }
@@ -36,7 +36,7 @@ class ProductRouteHandler
                     SEOTools::setDescription($product->meta_description);
                     SEOTools::opengraph()->setUrl(url()->current());
                     $metaImage = $product->meta_image;
-                    if (!$metaImage) {
+                    if (! $metaImage) {
                         $metaImage = $product->firstMediaUrl;
                     }
                     if ($metaImage) {
