53cd7d67909613f62fde724f84f0b3ea48ae4286
Robinvm <Robinvm@users.noreply.github.com>
2022-05-11 13:22:22 +0000

    Fix styling

--- src/Filament/Resources/ProductResource.php
+++ src/Filament/Resources/ProductResource.php
@@ -78,28 +78,28 @@ class ProductResource extends Resource
                     ->default('simple')
                     ->required()
                     ->reactive()
-                    ->hidden(fn($record) => $record && $record->parent_product_id),
+                    ->hidden(fn ($record) => $record && $record->parent_product_id),
                 Select::make('parent_product_id')
                     ->label('Bovenliggende product')
                     ->options(Product::where('type', 'variable')->whereNull('parent_product_id')->pluck('name', 'id'))
                     ->reactive()
                     ->searchable()
-                    ->hidden(fn(\Closure $get, $livewire) => $get('type') != 'variable' || $livewire instanceof EditProduct),
+                    ->hidden(fn (\Closure $get, $livewire) => $get('type') != 'variable' || $livewire instanceof EditProduct),
                 MultiSelect::make('site_ids')
                     ->label('Actief op sites')
                     ->options(collect(Sites::getSites())->pluck('name', 'id')->toArray())
                     ->default([Sites::getFirstSite()['id']])
-                    ->hidden(fn(\Closure $get) => !(Sites::getAmountOfSites() > 1) || $get('parent_product_id') && $get('type') == 'variable')
+                    ->hidden(fn (\Closure $get) => ! (Sites::getAmountOfSites() > 1) || $get('parent_product_id') && $get('type') == 'variable')
                     ->required()
-                    ->disabled(fn($record) => $record && $record->parent_product_id),
+                    ->disabled(fn ($record) => $record && $record->parent_product_id),
                 Toggle::make('public')
                     ->label('Openbaar')
                     ->default(1),
                 Toggle::make('only_show_parent_product')
                     ->label('Toon 1 variatie op overzichtspagina')
-                    ->hidden(fn($record, \Closure $get) => $get('type') != 'variable' || ($record && $record->parent_product_id)),
+                    ->hidden(fn ($record, \Closure $get) => $get('type') != 'variable' || ($record && $record->parent_product_id)),
             ])
-            ->collapsed(fn($livewire) => $livewire instanceof EditProduct);
+            ->collapsed(fn ($livewire) => $livewire instanceof EditProduct);
 
         $schema[] = Section::make('Voorraad beheren')
             ->schema([
@@ -109,7 +109,7 @@ class ProductResource extends Resource
                 TextInput::make('stock')
                     ->type('number')
                     ->label('Hoeveel heb je van dit product op voorraad')
-                    ->helperText(fn($record) => $record ? 'Er zijn er momenteel ' . $record->reservedStock() . ' gereserveerd' : '')
+                    ->helperText(fn ($record) => $record ? 'Er zijn er momenteel ' . $record->reservedStock() . ' gereserveerd' : '')
                     ->maxValue(100000)
                     ->required()
                     ->rules([
@@ -117,20 +117,20 @@ class ProductResource extends Resource
                         'numeric',
                         'max:100000',
                     ])
-                    ->hidden(fn(\Closure $get) => !$get('use_stock')),
+                    ->hidden(fn (\Closure $get) => ! $get('use_stock')),
                 Toggle::make('out_of_stock_sellable')
                     ->label('Product doorverkopen wanneer niet meer op voorraad (pre-orders)')
                     ->reactive()
-                    ->hidden(fn(\Closure $get) => !$get('use_stock')),
+                    ->hidden(fn (\Closure $get) => ! $get('use_stock')),
                 DatePicker::make('expected_in_stock_date')
                     ->label('Wanneer komt dit product weer op voorraad')
                     ->reactive()
                     ->required()
-                    ->hidden(fn(\Closure $get) => !$get('use_stock') || !$get('out_of_stock_sellable')),
+                    ->hidden(fn (\Closure $get) => ! $get('use_stock') || ! $get('out_of_stock_sellable')),
                 Toggle::make('low_stock_notification')
                     ->label('Ik wil een melding krijgen als dit product laag op voorraad raakt')
                     ->reactive()
-                    ->hidden(fn(\Closure $get) => !$get('use_stock')),
+                    ->hidden(fn (\Closure $get) => ! $get('use_stock')),
                 TextInput::make('low_stock_notification_limit')
                     ->label('Als de voorraad van dit product onder onderstaand nummer komt, krijg je een notificatie')
                     ->type('number')
@@ -146,7 +146,7 @@ class ProductResource extends Resource
                         'min:1',
                         'max:100000',
                     ])
-                    ->hidden(fn(\Closure $get) => !$get('use_stock') || !$get('low_stock_notification')),
+                    ->hidden(fn (\Closure $get) => ! $get('use_stock') || ! $get('low_stock_notification')),
                 Select::make('stock_status')
                     ->label('Is dit product op voorraad')
                     ->options([
@@ -158,7 +158,7 @@ class ProductResource extends Resource
                     ->rules([
                         'required',
                     ])
-                    ->hidden(fn(\Closure $get) => $get('use_stock')),
+                    ->hidden(fn (\Closure $get) => $get('use_stock')),
                 Toggle::make('limit_purchases_per_customer')
                     ->label('Dit product mag maar een x aantal keer per bestelling gekocht worden')
                     ->reactive(),
@@ -175,9 +175,9 @@ class ProductResource extends Resource
                         'min:1',
                         'max:100000',
                     ])
-                    ->hidden(fn(\Closure $get) => !$get('limit_purchases_per_customer')),
+                    ->hidden(fn (\Closure $get) => ! $get('limit_purchases_per_customer')),
             ])
-            ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id))
+            ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id))
             ->collapsible();
 
         $productFilters = ProductFilter::with(['productFilterOptions'])->get();
@@ -192,15 +192,15 @@ class ProductResource extends Resource
                     'default' => 1,
                     'lg' => 2,
                 ])
-                ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && $record && $record->parent_product_id);
+                ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && $record && $record->parent_product_id);
             $productFilterSchema[] = Toggle::make("product_filter_{$productFilter->id}_use_for_variations")
                 ->label("$productFilter->name gebruiken voor variaties op de product pagina")
-                ->hidden(fn(\Closure $get) => !$get("product_filter_$productFilter->id"))
+                ->hidden(fn (\Closure $get) => ! $get("product_filter_$productFilter->id"))
                 ->columnSpan([
                     'default' => 1,
                     'lg' => 2,
                 ])
-                ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && $record && $record->parent_product_id);
+                ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && $record && $record->parent_product_id);
             foreach ($productFilter->productFilterOptions as $productFilterOption) {
                 $productFiltersSchema[] = Checkbox::make("product_filter_{$productFilter->id}_option_{$productFilterOption->id}")
                     ->label("$productFilter->name: $productFilterOption->name");
@@ -209,7 +209,7 @@ class ProductResource extends Resource
                 ->schema($productFiltersSchema)
                 ->collapsible()
                 ->collapsed()
-                ->hidden(fn(\Closure $get, $record) => !$get("product_filter_$productFilter->id") || ($get('type') == 'variable' && $record && !$record->parent_product_id));
+                ->hidden(fn (\Closure $get, $record) => ! $get("product_filter_$productFilter->id") || ($get('type') == 'variable' && $record && ! $record->parent_product_id));
         }
 
         $schema[] = Section::make('Filters beheren')
@@ -218,8 +218,8 @@ class ProductResource extends Resource
                 'default' => 1,
                 'lg' => 2,
             ])
-            ->hidden(fn($livewire) => $livewire instanceof CreateProduct)
-            ->collapsed(fn($livewire) => $livewire instanceof EditProduct);
+            ->hidden(fn ($livewire) => $livewire instanceof CreateProduct)
+            ->collapsed(fn ($livewire) => $livewire instanceof EditProduct);
 
         $productCharacteristics = ProductCharacteristics::orderBy('order', 'ASC')->get();
         $productCharacteristicSchema = [];
@@ -235,8 +235,8 @@ class ProductResource extends Resource
                 'default' => 1,
                 'lg' => 3,
             ])
-            ->hidden(fn($livewire, \Closure $get, $record) => $livewire instanceof CreateProduct || ($get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id)))
-            ->collapsed(fn($livewire) => $livewire instanceof EditProduct);
+            ->hidden(fn ($livewire, \Closure $get, $record) => $livewire instanceof CreateProduct || ($get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id)))
+            ->collapsed(fn ($livewire) => $livewire instanceof EditProduct);
 
         $schema[] = Section::make('Content beheren')
             ->schema([TextInput::make('name')
@@ -247,7 +247,7 @@ class ProductResource extends Resource
                     'max:255',]),
                 TextInput::make('slug')
                     ->label('Slug')
-                    ->unique('qcommerce__products', 'slug', fn($record) => $record)
+                    ->unique('qcommerce__products', 'slug', fn ($record) => $record)
                     ->helperText('Laat leeg om automatisch te laten genereren')
                     ->rules(['max:255',]),
                 TinyEditor::make('description')
@@ -260,36 +260,36 @@ class ProductResource extends Resource
                         'default' => 1,
                         'lg' => 2,
                     ])
-                    ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id)),
+                    ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id)),
                 Textarea::make('short_description')
                     ->label('Korte beschrijving')
                     ->rows(5)
                     ->maxLength(2500)
                     ->rules(['max:2500',])
-                    ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id)),
+                    ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id)),
                 Textarea::make('search_terms')
                     ->label('Zoekwoorden')
                     ->rows(2)
                     ->helperText('Vul hier termen in waar het product nog meer op gevonden moet kunnen worden')
                     ->maxLength(2500)
                     ->rules(['max:2500',])
-                    ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id)),
+                    ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id)),
                 TextInput::make('meta_title')
                     ->label('Meta titel')
                     ->maxLength(100)
                     ->rules(['max:100',])
-                    ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id)),
+                    ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id)),
                 Textarea::make('meta_description')
                     ->label('Meta descriptie')
                     ->rows(2)
                     ->maxLength(200)
                     ->rules(['max:200',])
-                    ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id)),
+                    ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id)),
                 FileUpload::make('meta_image')
                     ->directory('qcommerce/products/meta-images')
                     ->name('Meta afbeelding')
                     ->image()
-                    ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id)),
+                    ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id)),
                 TextInput::make('order')
                     ->label('Volgorde')
                     ->required()
@@ -305,7 +305,7 @@ class ProductResource extends Resource
                     ->blocks(cms()->builder('blocks'))
                     ->columnSpan(['default' => 1,
                         'lg' => 2,])
-                    ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id)),])
+                    ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id)),])
             ->columns([
                 'default' => 1,
                 'lg' => 2,
@@ -413,7 +413,7 @@ class ProductResource extends Resource
                     ]),])
             ->columns(['default' => 1,
                 'lg' => 2,])
-            ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id))
+            ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id))
             ->collapsible();
 
         $schema[] = Section::make('Linkjes beheren')
@@ -435,7 +435,7 @@ class ProductResource extends Resource
                 'default' => 1,
                 'lg' => 2,
             ])
-            ->hidden(fn($record, \Closure $get) => $get('type') == 'variable' && (!$record && !$get('parent_product_id') || $record && !$record->parent_product_id))
+            ->hidden(fn ($record, \Closure $get) => $get('type') == 'variable' && (! $record && ! $get('parent_product_id') || $record && ! $record->parent_product_id))
             ->collapsible();
 
         $schema[] = Section::make('Product extras')
@@ -489,7 +489,7 @@ class ProductResource extends Resource
                             ]),
                     ]),
             ])
-            ->hidden(fn($livewire) => $livewire instanceof CreateProduct)
+            ->hidden(fn ($livewire) => $livewire instanceof CreateProduct)
             ->collapsible()
             ->collapsed();
 
@@ -519,7 +519,7 @@ class ProductResource extends Resource
                 TagsColumn::make('site_ids')
                     ->label('Actief op site(s)')
                     ->sortable()
-                    ->hidden(!(Sites::getAmountOfSites() > 1))
+                    ->hidden(! (Sites::getAmountOfSites() > 1))
                     ->searchable(),
                 TextColumn::make('total_purchases')
                     ->label('Aantal verkopen'),
