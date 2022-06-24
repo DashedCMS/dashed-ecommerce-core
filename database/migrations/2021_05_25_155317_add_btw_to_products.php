e573aaceee1c781fb5a93ef0d179cd885ce37f26
Robin van Maasakker <robin@qubiqx.com>
2022-06-24 08:17:27 +0200

    Update

--- database/migrations/2021_05_25_155317_add_btw_to_products.php
+++ database/migrations/2021_05_25_155317_add_btw_to_products.php
@@ -16,6 +16,12 @@ class AddBtwToProducts extends Migration
         Schema::table('qcommerce__order_products', function (Blueprint $table) {
             $table->decimal('btw')->after('price')->default(0);
         });
+
+//        foreach (\Qubiqx\QcommerceEcommerceCore\Models\OrderProduct::get() as $orderProduct) {
+//            $vatPrice = $orderProduct->price / (100 + ($orderProduct->product->vat_rate ?? 21)) * ($orderProduct->product->vat_rate ?? 21);
+//            $orderProduct->btw = $vatPrice;
+//            $orderProduct->save();
+//        }
     }
 
     /**
--- database/migrations/2021_11_09_133633_change_payment_and_shipping_costs_to_order_products.php
+++ database/migrations/2021_11_09_133633_change_payment_and_shipping_costs_to_order_products.php
@@ -21,46 +21,46 @@ class ChangePaymentAndShippingCostsToOrderProducts extends Migration
             $table->string('sku')->nullable()->after('product_id');
         });
 
-        foreach (Order::get() as $order) {
-            foreach ($order->orderProducts as $orderProduct) {
-                $orderProduct->sku = $orderProduct->product ? $orderProduct->product->sku : Str::slug($orderProduct->name);
-                $orderProduct->save();
-            }
-
-            if ($order->payment_costs > 0.00) {
-                $orderProduct = new OrderProduct();
-                $orderProduct->quantity = 1;
-                $orderProduct->product_id = null;
-                $orderProduct->order_id = $order->id;
-                $orderProduct->name = $order->paymentMethod ? $order->paymentMethod->name : $order->payment_method;
-                $orderProduct->price = $order->payment_costs;
-                $orderProduct->discount = 0;
-                $orderProduct->product_extras = json_encode([]);
-                $orderProduct->sku = 'payment_costs';
-                $orderProduct->btw = $orderProduct->price / 121 * 21;
-                $orderProduct->save();
-
-                $order->payment_costs = 0;
-                $order->save();
-            }
-
-            if ($order->shipping_costs > 0.00) {
-                $orderProduct = new OrderProduct();
-                $orderProduct->quantity = 1;
-                $orderProduct->product_id = null;
-                $orderProduct->order_id = $order->id;
-                $orderProduct->name = $order->shippingMethod->name;
-                $orderProduct->price = $order->shipping_costs;
-                $orderProduct->discount = 0;
-                $orderProduct->product_extras = json_encode([]);
-                $orderProduct->sku = 'shipping_costs';
-                $orderProduct->btw = 0;
-                $orderProduct->save();
-
-                $order->shipping_costs = 0;
-                $order->save();
-            }
-        }
+//        foreach (Order::get() as $order) {
+//            foreach ($order->orderProducts as $orderProduct) {
+//                $orderProduct->sku = $orderProduct->product ? $orderProduct->product->sku : Str::slug($orderProduct->name);
+//                $orderProduct->save();
+//            }
+//
+//            if ($order->payment_costs > 0.00) {
+//                $orderProduct = new OrderProduct();
+//                $orderProduct->quantity = 1;
+//                $orderProduct->product_id = null;
+//                $orderProduct->order_id = $order->id;
+//                $orderProduct->name = $order->paymentMethod ? $order->paymentMethod->name : $order->payment_method;
+//                $orderProduct->price = $order->payment_costs;
+//                $orderProduct->discount = 0;
+//                $orderProduct->product_extras = json_encode([]);
+//                $orderProduct->sku = 'payment_costs';
+//                $orderProduct->btw = $orderProduct->price / 121 * 21;
+//                $orderProduct->save();
+//
+//                $order->payment_costs = 0;
+//                $order->save();
+//            }
+//
+//            if ($order->shipping_costs > 0.00) {
+//                $orderProduct = new OrderProduct();
+//                $orderProduct->quantity = 1;
+//                $orderProduct->product_id = null;
+//                $orderProduct->order_id = $order->id;
+//                $orderProduct->name = $order->shippingMethod->name;
+//                $orderProduct->price = $order->shipping_costs;
+//                $orderProduct->discount = 0;
+//                $orderProduct->product_extras = json_encode([]);
+//                $orderProduct->sku = 'shipping_costs';
+//                $orderProduct->btw = 0;
+//                $orderProduct->save();
+//
+//                $order->shipping_costs = 0;
+//                $order->save();
+//            }
+//        }
 
         Schema::table('qcommerce__orders', function (Blueprint $table) {
             $table->dropColumn('payment_costs');
