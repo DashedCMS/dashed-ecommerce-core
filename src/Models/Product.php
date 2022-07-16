207c1a490ba82e2d15336cc29966c0c9866ad2ac
Robin van Maasakker <robin@qubiqx.com>
2022-07-16 16:36:22 +0200

    Update

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -507,7 +507,7 @@ class Product extends Model
                         'productId' => ($this->id == $childProduct->id) ? $this->id : '',
                         'in_stock' => ($this->id == $childProduct->id) ? $this->inStock() : false,
                         'inStock' => ($this->id == $childProduct->id) ? $this->inStock() : false,
-                        'directSellableStock' => ($this->id == $childProduct->id) ? $this->hasDirectSellableStock() : false,
+                        'isPreOrder' => ($this->id == $childProduct->id) ? $this->isPreorderable() : false,
                     ];
                     if ($this->id == $childProduct->id) {
                         $activeFiltersValues[$activeFilterId] = [
@@ -519,7 +519,7 @@ class Product extends Model
                             'productId' => ($this->id == $childProduct->id) ? $this->id : '',
                             'in_stock' => ($this->id == $childProduct->id) ? $this->inStock() : false,
                             'inStock' => ($this->id == $childProduct->id) ? $this->inStock() : false,
-                            'directSellableStock' => ($this->id == $childProduct->id) ? $this->hasDirectSellableStock() : false,
+                            'isPreOrder' => ($this->id == $childProduct->id) ? ($this->isPreorderable()) : false,
                         ];
                     }
                 }
