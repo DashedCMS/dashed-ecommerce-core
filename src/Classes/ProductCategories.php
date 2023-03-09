01dafdfdb7f5a7083e345df3298833559be836ec
Robin van Maasakker <robin@qubiqx.com>
2023-03-09 10:15:55 +0100

    Update

--- src/Classes/ProductCategories.php
+++ src/Classes/ProductCategories.php
@@ -15,7 +15,7 @@ class ProductCategories
     public static function getTopLevel($limit = 4, $orderBy = 'created_at', $order = 'DESC')
     {
 //        return Cache::tags(['product-categories'])->rememberForever("product-categories-top-level-$limit-$orderBy-$order", function () use ($limit, $orderBy, $order) {
-        return ProductCategory::with(['products'])->where('parent_category_id', null)->limit($limit)->orderBy($orderBy, $order)->get();
+        return ProductCategory::with(['products'])->where('parent_id', null)->limit($limit)->orderBy($orderBy, $order)->get();
 //        });
     }
 
