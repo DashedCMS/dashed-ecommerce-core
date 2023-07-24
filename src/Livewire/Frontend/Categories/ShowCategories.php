3e49144f49bdf8c979069ce9699fe02c581150b2
Robin van Maasakker <robin@qubiqx.com>
2023-07-24 20:04:31 +0200

    Update

--- src/Livewire/Frontend/Categories/ShowCategories.php
+++ src/Livewire/Frontend/Categories/ShowCategories.php
@@ -2,12 +2,20 @@
 
 namespace Qubiqx\QcommerceEcommerceCore\Livewire\Frontend\Categories;
 
+use Illuminate\Database\Eloquent\Collection;
 use Livewire\Component;
+use Qubiqx\QcommerceEcommerceCore\Classes\ProductCategories;
+use Qubiqx\QcommerceEcommerceCore\Models\ProductCategory;
 
 class ShowCategories extends Component
 {
-    public function mount()
+    public ?Collection $productCategories = null;
+    public ?ProductCategory $productCategory = null;
+
+    public function mount(?Collection $productCategories = null, ?ProductCategory $productCategory = null)
     {
+        $this->productCategories = $productCategories ?: ProductCategories::getTopLevel(100);
+        $this->productCategory = $productCategory;
     }
 
     public function render()
