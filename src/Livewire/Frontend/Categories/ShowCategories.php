64314e70d3c63723403ed5acf7bef597689d999a
Robinvm <Robinvm@users.noreply.github.com>
2023-07-24 17:50:43 +0000

    Fix styling

--- src/Livewire/Frontend/Categories/ShowCategories.php
+++ src/Livewire/Frontend/Categories/ShowCategories.php
@@ -3,15 +3,9 @@
 namespace Qubiqx\QcommerceEcommerceCore\Livewire\Frontend\Categories;
 
 use Livewire\Component;
-use Qubiqx\QcommerceEcommerceCore\Models\Product;
-use Qubiqx\QcommerceTranslations\Models\Translation;
-use Qubiqx\QcommerceEcommerceCore\Models\DiscountCode;
-use Qubiqx\QcommerceEcommerceCore\Classes\ShoppingCart;
-use Qubiqx\QcommerceEcommerceCore\Livewire\Concerns\CartActions;
 
 class ShowCategories extends Component
 {
-
     public function mount()
     {
     }
