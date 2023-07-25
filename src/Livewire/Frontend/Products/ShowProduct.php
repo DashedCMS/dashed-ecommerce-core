f73b43de83b0dcd603345cb4737d1c750ecd53b7
Robinvm <Robinvm@users.noreply.github.com>
2023-07-25 10:00:05 +0000

    Fix styling

--- src/Livewire/Frontend/Products/ShowProduct.php
+++ src/Livewire/Frontend/Products/ShowProduct.php
@@ -2,15 +2,8 @@
 
 namespace Qubiqx\QcommerceEcommerceCore\Livewire\Frontend\Products;
 
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Collection;
 use Livewire\Component;
-use Livewire\WithPagination;
-use Qubiqx\QcommerceCore\Models\Customsetting;
-use Qubiqx\QcommerceEcommerceCore\Classes\ProductCategories;
-use Qubiqx\QcommerceEcommerceCore\Classes\Products;
 use Qubiqx\QcommerceEcommerceCore\Models\Product;
-use Qubiqx\QcommerceEcommerceCore\Models\ProductCategory;
 
 class ShowProduct extends Component
 {
@@ -24,6 +17,7 @@ class ShowProduct extends Component
         $this->characteristics = $product->showableCharacteristics();
         $this->suggestedProducts = $product->getSuggestedProducts();
     }
+
     public function render()
     {
         return view('qcommerce-ecommerce-core::frontend.products.show-product');
