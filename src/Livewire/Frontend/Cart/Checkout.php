b4f9887d5fc2e761874b7170da390a45c82638c1
Robinvm <Robinvm@users.noreply.github.com>
2023-07-26 14:40:47 +0000

    Fix styling

--- src/Livewire/Frontend/Cart/Checkout.php
+++ src/Livewire/Frontend/Cart/Checkout.php
@@ -106,7 +106,7 @@ class Checkout extends Component
     public function retrievePaymentMethods()
     {
         $this->paymentMethods = $this->country ? ShoppingCart::getAvailablePaymentMethods($this->country, true) : [];
-        if (!$this->paymentMethod && count($this->paymentMethods)) {
+        if (! $this->paymentMethod && count($this->paymentMethods)) {
             $this->paymentMethod = $this->paymentMethods[0]['id'] ?? '';
         }
     }
@@ -116,7 +116,7 @@ class Checkout extends Component
         $shippingAddress = "$this->street $this->houseNr, $this->zipCode $this->city, $this->country";
 
         $this->shippingMethods = $this->country ? ShoppingCart::getAvailableShippingMethods($this->country, true, $shippingAddress) : [];
-        if (!$this->shippingMethod && count($this->shippingMethods)) {
+        if (! $this->shippingMethod && count($this->shippingMethods)) {
             $this->shippingMethod = $this->shippingMethods->first()['id'] ?? '';
         }
     }
@@ -212,7 +212,7 @@ class Checkout extends Component
                 'max:255',
             ],
             'password' => [
-                Rule::requiredIf(Customsetting::get('checkout_account') == 'required' && !auth()->check()),
+                Rule::requiredIf(Customsetting::get('checkout_account') == 'required' && ! auth()->check()),
                 'nullable',
                 'min:6',
                 'max:255',
@@ -322,7 +322,7 @@ class Checkout extends Component
 
         $cartItems = $this->cartItems;
 
-        if (!$cartItems) {
+        if (! $cartItems) {
             return $this->emit('showAlert', 'error', Translation::get('no-items-in-cart', 'cart', 'You dont have any products in your shopping cart'));
         }
 
@@ -335,13 +335,13 @@ class Checkout extends Component
         }
 
         $paymentMethodPresent = (bool)$paymentMethod;
-        if (!$paymentMethodPresent) {
+        if (! $paymentMethodPresent) {
             foreach (ecommerce()->builder('paymentServiceProviders') as $psp) {
                 if ($psp['class']::isConnected()) {
                     $paymentMethodPresent = true;
                 }
             }
-            if (!$paymentMethodPresent) {
+            if (! $paymentMethodPresent) {
                 return $this->emit('showAlert', 'error', Translation::get('no-valid-payment-method-chosen', 'cart', 'You did not choose a valid payment method'));
             }
         }
@@ -354,7 +354,7 @@ class Checkout extends Component
             }
         }
 
-        if (!$shippingMethod) {
+        if (! $shippingMethod) {
             return $this->emit('showAlert', 'error', Translation::get('no-valid-shipping-method-chosen', 'cart', 'You did not choose a valid shipping method'));
         }
 
@@ -377,17 +377,17 @@ class Checkout extends Component
                 }
             }
 
-            if (!$depositPaymentMethod) {
+            if (! $depositPaymentMethod) {
                 return $this->emit('showAlert', 'error', Translation::get('no-valid-deposit-payment-method-chosen', 'cart', 'You did not choose a valid payment method for the deposit'));
             }
         }
 
         $discountCode = DiscountCode::usable()->where('code', session('discountCode'))->first();
 
-        if (!$discountCode) {
+        if (! $discountCode) {
             session(['discountCode' => '']);
             $discountCode = '';
-        } elseif ($discountCode && !$discountCode->isValidForCart($this->email)) {
+        } elseif ($discountCode && ! $discountCode->isValidForCart($this->email)) {
             session(['discountCode' => '']);
 
             return $this->emit('showAlert', 'error', Translation::get('discount-code-invalid', 'cart', 'The discount code you choose is invalid'));
@@ -572,7 +572,7 @@ class Checkout extends Component
 
         $orderPayment->psp = $psp;
 
-        if (!$paymentMethod) {
+        if (! $paymentMethod) {
             $orderPayment->payment_method = $psp;
         } elseif ($orderPayment->psp == 'own') {
             $orderPayment->payment_method_id = $paymentMethod['id'];
