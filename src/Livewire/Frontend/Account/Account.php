19522b7bdb49bc17961e58743c8bedd3fd184faf
Robinvm <Robinvm@users.noreply.github.com>
2023-07-27 11:59:05 +0000

    Fix styling

--- src/Livewire/Frontend/Account/Account.php
+++ src/Livewire/Frontend/Account/Account.php
@@ -2,26 +2,7 @@
 
 namespace Qubiqx\QcommerceEcommerceCore\Livewire\Frontend\Account;
 
-use Exception;
-use Carbon\Carbon;
 use Livewire\Component;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Http;
-use Qubiqx\QcommerceCore\Models\User;
-use Illuminate\Support\Facades\Validator;
-use Qubiqx\QcommerceCore\Models\Customsetting;
-use Qubiqx\QcommerceEcommerceCore\Models\Order;
-use Qubiqx\QcommerceEcommerceCore\Models\Product;
-use Qubiqx\QcommerceEcommerceCore\Models\OrderLog;
-use Qubiqx\QcommerceTranslations\Models\Translation;
-use Qubiqx\QcommerceEcommerceCore\Models\DiscountCode;
-use Qubiqx\QcommerceEcommerceCore\Models\OrderPayment;
-use Qubiqx\QcommerceEcommerceCore\Models\OrderProduct;
-use Qubiqx\QcommerceEcommerceCore\Classes\ShoppingCart;
-use Qubiqx\QcommerceEcommerceCore\Models\ProductExtraOption;
-use Qubiqx\QcommerceEcommerceCore\Livewire\Concerns\CartActions;
 
 class Account extends Component
 {
