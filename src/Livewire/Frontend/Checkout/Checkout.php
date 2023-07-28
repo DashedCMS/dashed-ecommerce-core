a126b1514f47888af150f43f17a9602eb6bab0eb
Robinvm <Robinvm@users.noreply.github.com>
2023-07-28 08:31:04 +0000

    Fix styling

--- src/Livewire/Frontend/Checkout/Checkout.php
+++ src/Livewire/Frontend/Checkout/Checkout.php
@@ -2,26 +2,26 @@
 
 namespace Qubiqx\QcommerceEcommerceCore\Livewire\Frontend\Checkout;
 
-use Carbon\Carbon;
 use Exception;
+use Carbon\Carbon;
+use Livewire\Component;
+use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
-use Livewire\Component;
-use Qubiqx\QcommerceCore\Models\Customsetting;
 use Qubiqx\QcommerceCore\Models\User;
-use Qubiqx\QcommerceEcommerceCore\Classes\ShoppingCart;
-use Qubiqx\QcommerceEcommerceCore\Livewire\Concerns\CartActions;
-use Qubiqx\QcommerceEcommerceCore\Models\DiscountCode;
+use Illuminate\Support\Facades\Validator;
+use Qubiqx\QcommerceCore\Models\Customsetting;
 use Qubiqx\QcommerceEcommerceCore\Models\Order;
+use Qubiqx\QcommerceEcommerceCore\Models\Product;
 use Qubiqx\QcommerceEcommerceCore\Models\OrderLog;
+use Qubiqx\QcommerceTranslations\Models\Translation;
+use Qubiqx\QcommerceEcommerceCore\Models\DiscountCode;
 use Qubiqx\QcommerceEcommerceCore\Models\OrderPayment;
 use Qubiqx\QcommerceEcommerceCore\Models\OrderProduct;
-use Qubiqx\QcommerceEcommerceCore\Models\Product;
+use Qubiqx\QcommerceEcommerceCore\Classes\ShoppingCart;
 use Qubiqx\QcommerceEcommerceCore\Models\ProductExtraOption;
-use Qubiqx\QcommerceTranslations\Models\Translation;
+use Qubiqx\QcommerceEcommerceCore\Livewire\Concerns\CartActions;
 
 class Checkout extends Component
 {
