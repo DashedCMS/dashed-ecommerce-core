5612f3f94534b1c6103aeeae0baaab8a5e783920
Robin van Maasakker <robin@qubiqx.com>
2023-07-28 10:21:38 +0200

    Update

--- resources/views/frontend/account/orders.blade.php
+++ resources/views/frontend/account/orders.blade.php
@@ -1,5 +1,5 @@
 <x-container>
-    <div class="grid grid-cols-6 gap-8">
+    <div class="grid grid-cols-6 gap-8 py-16 sm:py-24">
         <div class="col-span-4 lg:col-span-2">
             <nav class="grid space-y-2" aria-label="Tabs">
                 <a href="{{AccountHelper::getAccountUrl()}}"
@@ -17,23 +17,23 @@
             </nav>
         </div>
         <div class="col-span-6 lg:col-span-4">
-            <h1 class="text-2xl">{{Translation::get('my-orders', 'account', 'Mijn bestellingen')}}</h1>
-            @if(!Auth::user()->orders->count())
-                <p>{{Translation::get('no-orders-yet', 'account', 'U heeft nog geen bestellingen gedaan')}}</p>
+            <h1 class="text-2xl">{{Translation::get('my-orders', 'account', 'My orders')}}</h1>
+            @if(!$orders->count())
+                <p>{{Translation::get('no-orders-yet', 'account', 'You dont have any orders yet')}}</p>
             @else
                 <div class="mt-8 space-y-8">
-                    @foreach(Auth::user()->orders as $order)
-                        <div class="space-y-6 shadow-xl rounded-md bg-gray-50 p-4">
+                    @foreach($orders as $order)
+                        <div class="space-y-6 shadow-xl text-white rounded-md bg-gradient-to-tr from-primary-200/50 to-primary-300 p-4">
                             <div class="md:flex items-center justify-between">
                                 <p>{{$order->created_at->format('d-m-Y')}} | {{$order->invoice_id}}</p>
                                 <div class="space-y-2">
-                                    <a class="button button--primary px-3 py-1.5 md:px-6 md:py-3 text-sm md:text-lg"
+                                    <a class="button button-primary-on-white"
                                        href="{{$order->downloadInvoiceUrl()}}">
-                                        {{Translation::get('download-invoice', 'cart', 'Factuur downloaden')}}
+                                        {{Translation::get('download-invoice', 'cart', 'Download invoice')}}
                                     </a>
-                                    <a class="button button--primary px-3 py-1.5 md:px-6 md:py-3 text-sm md:text-lg"
+                                    <a class="button button-primary-on-white"
                                        href="{{$order->getUrl()}}">
-                                        {{Translation::get('view-order', 'cart', 'Bestelling bekijken')}}
+                                        {{Translation::get('view-order', 'cart', 'View order')}}
                                     </a>
                                 </div>
                             </div>
@@ -44,30 +44,31 @@
                                        class="flex py-4 space-x-4">
                                         @if($orderProduct->product && $orderProduct->product->firstImageUrl)
                                             <x-drift::image
-                                                    class="object-cover h-16"
-                                                    config="qcommerce"
-                                                    :path="$orderProduct->product->firstImageUrl"
-                                                    :alt=" $orderProduct->product->name"
-                                                    :manipulations="[
+                                                class="object-cover h-16"
+                                                config="qcommerce"
+                                                :path="$orderProduct->product->firstImageUrl"
+                                                :alt=" $orderProduct->product->name"
+                                                :manipulations="[
                                                     'widen' => 150,
                                                 ]"
                                             />
                                         @endif
 
                                         <article>
-                                            <h3 class="text-sm md:text-2xl font-display line-clamp-3">{{$orderProduct->name}}
+                                            <h3 class="text-sm md:text-2xl font-bold line-clamp-5">{{$orderProduct->name}}
                                                 @if($orderProduct->product_extras)
-                                                    @foreach($orderProduct->product_extras as $option)
-                                                        <br>
-                                                        <small>{{$option['name']}}: {{$option['value']}}</small>
-                                                    @endforeach
+                                                    <div>
+                                                        @foreach($orderProduct->product_extras as $option)
+                                                            <p class="text-xs">{{$option['name']}}: {{$option['value']}}</p>
+                                                        @endforeach
+                                                    </div>
                                                 @endif
                                             </h3>
                                             <p class="text-lg font-bold">
                                                 {{CurrencyHelper::formatPrice($orderProduct->price)}}
-                                                |
+                                                <span>|</span>
                                                 {{$orderProduct->quantity}}
-                                                x
+                                                <span>x</span>
                                             </p>
                                         </article>
                                     </a>
