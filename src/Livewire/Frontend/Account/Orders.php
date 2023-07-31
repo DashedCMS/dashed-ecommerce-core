69eb69be481bad7e7935404fef73017d4a6c3e35
Robin van Maasakker <robin@qubiqx.com>
2023-07-31 11:25:36 +0200

    Update

--- src/Livewire/Frontend/Account/Orders.php
+++ src/Livewire/Frontend/Account/Orders.php
@@ -2,10 +2,18 @@
 
 namespace Qubiqx\QcommerceEcommerceCore\Livewire\Frontend\Account;
 
+use Illuminate\Database\Eloquent\Collection;
 use Livewire\Component;
 
 class Orders extends Component
 {
+    public Collection $orders;
+
+    public function mount()
+    {
+        $this->orders = auth()->user()->orders()->with('products')->get();
+    }
+
     public function render()
     {
         return view('qcommerce-ecommerce-core::frontend.account.orders');
