31f0e38638201072689cd55227a6b3a2574c2ea5
Robin van Maasakker <robin@qubiqx.com>
2023-11-17 11:13:04 +0100

    Update

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -37,6 +37,8 @@ class Product extends Model
         'search_terms',
         'content',
         'images',
+        'productCharacteristics',
+        'productExtras',
     ];
 
     protected $with = [
@@ -104,12 +106,14 @@ class Product extends Model
         $query->where(function ($query) use ($search) {
             $loop = 1;
             foreach (self::getTranslatableAttributes() as $attribute) {
-                if ($loop == 1) {
-                    $query->whereRaw('LOWER(`' . $attribute . '`) LIKE ? ', ['%' . trim(strtolower($search)) . '%']);
-                } else {
-                    $query->orWhereRaw('LOWER(`' . $attribute . '`) LIKE ? ', ['%' . trim(strtolower($search)) . '%']);
+                if(!method_exists($this, $attribute)){
+                    if ($loop == 1) {
+                        $query->whereRaw('LOWER(`' . $attribute . '`) LIKE ? ', ['%' . trim(strtolower($search)) . '%']);
+                    } else {
+                        $query->orWhereRaw('LOWER(`' . $attribute . '`) LIKE ? ', ['%' . trim(strtolower($search)) . '%']);
+                    }
+                    $loop++;
                 }
-                $loop++;
             }
         });
     }
