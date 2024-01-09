f2acc243ef94656a0c4d0190fd7bde1c6be1408d
Robinvm <Robinvm@users.noreply.github.com>
2024-01-09 08:02:29 +0000

    Fix styling

--- src/Classes/Products.php
+++ src/Classes/Products.php
@@ -36,11 +36,11 @@ class Products
             }
         }
 
-        if (!$orderBy) {
+        if (! $orderBy) {
             $orderBy = Customsetting::get('product_default_order_type', null, 'price');
         }
 
-        if (!$order) {
+        if (! $order) {
             $order = Customsetting::get('product_default_order_sort', null, 'DESC');
         }
 
@@ -118,14 +118,14 @@ class Products
                     foreach ($productFilter->productFilterOptions as $option) {
                         if ($option->checked) {
                             $filterIsActive = true;
-                            if (!$productValidForFilter) {
+                            if (! $productValidForFilter) {
                                 if ($product->productFilters()->where('product_filter_id', $productFilter->id)->where('product_filter_option_id', $option->id)->exists()) {
                                     $productValidForFilter = true;
                                 }
                             }
                         }
                     }
-                    if ($filterIsActive && !$productValidForFilter) {
+                    if ($filterIsActive && ! $productValidForFilter) {
                         $productIsValid = false;
                     }
                 }
@@ -178,7 +178,7 @@ class Products
                 $option->resultCount = 0;
                 if ($products) {
                     $option->resultCount = $option->resultCount + $option->products()->whereIn('product_id', $products)->count();
-                    if (!$filterHasActiveOptions && $option->resultCount > 0) {
+                    if (! $filterHasActiveOptions && $option->resultCount > 0) {
                         $filterHasActiveOptions = true;
                     }
                 }
@@ -356,14 +356,14 @@ class Products
                         if ($option->checked) {
                             //                            dump($option->name);
                             $filterIsActive = true;
-                            if (!$productValidForFilter) {
+                            if (! $productValidForFilter) {
                                 if ($product->productFilters()->where('product_filter_id', $productFilter->id)->where('product_filter_option_id', $option->id)->exists()) {
                                     $productValidForFilter = true;
                                 }
                             }
                         }
                     }
-                    if ($filterIsActive && !$productValidForFilter) {
+                    if ($filterIsActive && ! $productValidForFilter) {
                         $productIsValid = false;
                     }
                 }
@@ -422,7 +422,7 @@ class Products
                 $option->resultCount = 0;
                 if ($products) {
                     $option->resultCount = $option->resultCount + $option->products()->whereIn('product_id', $products)->count();
-                    if (!$filterHasActiveOptions && $option->resultCount > 0) {
+                    if (! $filterHasActiveOptions && $option->resultCount > 0) {
                         $filterHasActiveOptions = true;
                     }
                 }
