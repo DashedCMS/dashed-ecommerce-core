9b4b1294e33ae882cdf756ca34172c4e50fa6dba
Robinvm <Robinvm@users.noreply.github.com>
2024-04-04 06:50:41 +0000

    Fix styling

--- src/Filament/Pages/Settings/CheckoutSettingsPage.php
+++ src/Filament/Pages/Settings/CheckoutSettingsPage.php
@@ -2,7 +2,6 @@
 
 namespace Dashed\DashedEcommerceCore\Filament\Pages\Settings;
 
-use Filament\Forms\Components\Select;
 use Filament\Pages\Page;
 use Filament\Forms\Components\Tabs;
 use Dashed\DashedCore\Classes\Sites;
