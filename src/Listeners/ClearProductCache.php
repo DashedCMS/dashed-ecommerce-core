41737747d61e06bc1d8fc2b2ce1cdc555add45f8
Robin van Maasakker <robin@qubiqx.com>
2024-05-31 08:48:54 +0200

    Update

--- src/Listeners/ClearProductCache.php
+++ src/Listeners/ClearProductCache.php
@@ -2,6 +2,7 @@
 
 namespace Dashed\DashedEcommerceCore\Listeners;
 
+use Dashed\DashedEcommerceCore\Events\Products\ProductSavedEvent;
 use Illuminate\Support\Facades\Cache;
 use Dashed\DashedCore\Classes\Locales;
 use Dashed\DashedEcommerceCore\Events\Products\ProductUpdatedEvent;
@@ -19,12 +20,11 @@ class ClearProductCache
     /**
      * Handle the event.
      */
-    public function handle(ProductUpdatedEvent $event): void
+    public function handle(ProductSavedEvent $event): void
     {
         foreach (Locales::getLocalesArray() as $key => $locale) {
             Cache::forget('product-' . $event->product->id . '-url-' . $key);
         }
         Cache::forget("product-showable-characteristics-" . $event->product->id);
-        dd('asdf');
     }
 }
