cbbc5f07b60b46a364c5b01bf3d8666224788c89
Robin van Maasakker <robin@qubiqx.com>
2024-07-18 22:37:10 +0200

    Update

--- src/Models/ProductCategory.php
+++ src/Models/ProductCategory.php
@@ -85,7 +85,7 @@ class ProductCategory extends Model
 
     public function getUrl()
     {
-        if (! $this->childs->count()) {
+        if (!$this->childs->count()) {
             if ($this->products->count() == 1) {
                 return $this->products->first()->getUrl();
             } else {
@@ -152,7 +152,7 @@ class ProductCategory extends Model
         if ($productCategory) {
             array_shift($slugComponents);
             foreach ($slugComponents as $slugComponent) {
-                if (! $productCategory) {
+                if (!$productCategory) {
                     return 'pageNotFound';
                 }
                 $productCategory = ProductCategory::thisSite()->slug($slugComponent)->where('parent_id', $productCategory->id)->first();
@@ -164,8 +164,14 @@ class ProductCategory extends Model
                     seo()->metaData('metaImage', $productCategory->metadata->image);
                 }
 
-                View::share('productCategory', $productCategory);
+//                View::share('productCategory', $productCategory);
 
+                return [
+                    'view' => Customsetting::get('site_theme', null, 'dashed') . '.categories.show',
+                    'parameters' => [
+                        'productCategory' => $productCategory,
+                    ],
+                ];
                 return view(Customsetting::get('site_theme', null, 'dashed') . '.categories.show');
             } else {
                 return 'pageNotFound';
@@ -177,10 +183,17 @@ class ProductCategory extends Model
                 if (View::exists(Customsetting::get('site_theme', null, 'dashed') . '.categories.index')) {
                     seo()->metaData('metaTitle', Translation::get('all-categories', 'categories', 'All categories'));
 
-                    View::share('productCategory', null);
+//                    View::share('productCategory', null);
                     $childProductCategories = ProductCategories::getTopLevel(1000);
-                    View::share('childProductCategories', $childProductCategories);
+//                    View::share('childProductCategories', $childProductCategories);
 
+                    return [
+                        'view' => Customsetting::get('site_theme', null, 'dashed') . '.categories.index',
+                        'parameters' => [
+                            'productCategory' => null,
+                            'childProductCategories' => $childProductCategories,
+                        ],
+                    ];
                     return view(Customsetting::get('site_theme', null, 'dashed') . '.categories.index');
                 } else {
                     return 'pageNotFound';
@@ -200,10 +213,17 @@ class ProductCategory extends Model
                     if (View::exists(Customsetting::get('site_theme', null, 'dashed') . '.categories.index') && $productCategory) {
                         seo()->metaData('metaTitle', $productCategory->name);
 
-                        View::share('productCategory', $productCategory);
+//                        View::share('productCategory', $productCategory);
                         $childProductCategories = $productCategory->getFirstChilds();
-                        View::share('childProductCategories', $childProductCategories);
+//                        View::share('childProductCategories', $childProductCategories);
 
+                        return [
+                            'view' => Customsetting::get('site_theme', null, 'dashed') . '.categories.index',
+                            'parameters' => [
+                                'productCategory' => $productCategory,
+                                'childProductCategories' => $childProductCategories,
+                            ],
+                        ];
                         return view(Customsetting::get('site_theme', null, 'dashed') . '.categories.index');
                     } else {
                         return 'pageNotFound';
