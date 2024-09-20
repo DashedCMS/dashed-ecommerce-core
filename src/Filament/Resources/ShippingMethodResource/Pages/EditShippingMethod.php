6f59b44965fcdad521f0c162982a735fffe42b63
Robin van Maasakker <robin@qubiqx.com>
2024-09-20 16:01:09 +0200

    Update

--- src/Filament/Resources/ShippingMethodResource/Pages/EditShippingMethod.php
+++ src/Filament/Resources/ShippingMethodResource/Pages/EditShippingMethod.php
@@ -38,13 +38,13 @@ class EditShippingMethod extends EditRecord
     protected function mutateFormDataBeforeSave($data): array
     {
         foreach (ShippingClass::get() as $shippingClass) {
-            //            dump($shippingClass->id);
+//                        dump($shippingClass->id);
             if (isset($data["shipping_class_costs_$shippingClass->id"])) {
                 $value = $data["shipping_class_costs_$shippingClass->id"];
 
                 $shippingMethodClass = $this->record->shippingMethodClasses()->where('shipping_class_id', $shippingClass->id)->first();
 
-                if (! $shippingMethodClass) {
+                if (!$shippingMethodClass) {
                     $shippingMethodClass = $this->record->shippingMethodClasses()->create([
                         'shipping_class_id' => $shippingClass->id,
                     ]);
@@ -53,10 +53,10 @@ class EditShippingMethod extends EditRecord
                 $shippingMethodClass->costs = $value;
                 $shippingMethodClass->save();
                 //                $this->form->getState()["shipping_class_costs_$shippingClass->id"] = null;
-                unset($data["shipping_class_costs_$shippingClass->id"]);
                 //                dump($this->form->getState()["shipping_class_costs_$shippingClass->id"]);
                 //                unset($this->form->getState()["shipping_class_costs_$shippingClass->id"]);
             }
+            unset($data["shipping_class_costs_$shippingClass->id"]);
         }
 
         return $data;
