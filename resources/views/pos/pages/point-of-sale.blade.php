61f1e20c64377526288cc79552ea3600ff22a706
Robin van Maasakker <robin@qubiqx.com>
2024-09-22 19:41:49 +0200

    Update

--- resources/views/pos/pages/point-of-sale.blade.php
+++ resources/views/pos/pages/point-of-sale.blade.php
@@ -1,2 +1,13 @@
-<x-filament::page>
-</x-filament::page>
+<div class="px-8 py-16">
+    <div>
+        <p class="font-bold text-5xl">{{ Customsetting::get('site_name') }}</p>
+    </div>
+    <div class="grid grid-cols-8">
+        <div class="sm:col-span-5">
+
+        </div>
+        <div class="sm:col-span-3">
+
+        </div>
+    </div>
+</div>
--- src/Filament/Pages/POS/POSPage.php
+++ src/Filament/Pages/POS/POSPage.php
@@ -2,6 +2,8 @@
 
 namespace Dashed\DashedEcommerceCore\Filament\Pages\POS;
 
+use Dashed\DashedEcommerceCore\Classes\CurrencyHelper;
+use Dashed\DashedEcommerceCore\Classes\ShoppingCart;
 use Filament\Pages\Page;
 use Illuminate\Database\Eloquent\Collection;
 use Dashed\DashedEcommerceCore\Models\Product;
@@ -19,10 +21,59 @@ class POSPage extends page
     protected static string $view = 'dashed-ecommerce-core::pos.pages.point-of-sale';
 
     public Collection $products;
+    public $subTotal = 0;
+    public $discount = 0;
+    public $vat = 0;
+    public $total = 0;
+    public $totalUnformatted = 0;
+
+    public $user_id;
+    public $marketing;
+    public $password;
+    public $password_confirmation;
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $phone_number;
+    public $date_of_birth;
+    public $gender;
+    public $street;
+    public $house_nr;
+    public $zip_code;
+    public $city;
+    public $country;
+    public $company_name;
+    public $btw_id;
+    public $invoice_street;
+    public $invoice_house_nr;
+    public $invoice_zip_code;
+    public $invoice_city;
+    public $invoice_country;
+    public $note;
+    public $discount_code;
+    public ?string $activeDiscountCode = '';
+    public $orderProducts = [];
+    public $shipping_method_id;
+    public $payment_method_id;
+    public $activatedProducts = [];
 
     public function mount(): void
     {
-        $this->products = Product::publicShowable()->thisSite()->get();
+        $this->products = $this->getAllProducts();
+        ShoppingCart::setInstance('point-of-sale');
+    }
+
+    public function getAllProducts()
+    {
+        $products = Product::handOrderShowable()->with(['childProducts', 'parent'])->get();
+
+        foreach ($products as &$product) {
+            $product['stock'] = $product->stock();
+            $product['price'] = CurrencyHelper::formatPrice($product->price);
+            $product['productExtras'] = $product->allProductExtras();
+        }
+
+        return $products;
     }
 
     public function updated()
