eeb7192f35fb8640c99a8c32c46e1ffe715b8546
Robinvm <Robinvm@users.noreply.github.com>
2024-09-24 08:14:02 +0000

    Styling fixed with PHP CS Fixer

--- src/Filament/Resources/OrderResource/Concerns/CreateManualOrderActions.php
+++ src/Filament/Resources/OrderResource/Concerns/CreateManualOrderActions.php
@@ -3,14 +3,11 @@
 namespace Dashed\DashedEcommerceCore\Filament\Resources\OrderResource\Concerns;
 
 use Carbon\Carbon;
-use Dashed\DashedCore\Classes\Sites;
-use Dashed\DashedEcommerceCore\Models\PaymentMethod;
-use Filament\Actions\Concerns\HasForm;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Form;
 use Filament\Forms\Get;
+use Filament\Forms\Form;
 use Dashed\DashedCore\Models\User;
 use Illuminate\Support\Facades\DB;
+use Dashed\DashedCore\Classes\Sites;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Filament\Forms\Components\Select;
@@ -24,12 +21,14 @@ use Dashed\DashedEcommerceCore\Models\Order;
 use Filament\Forms\Components\DateTimePicker;
 use Dashed\DashedEcommerceCore\Models\Product;
 use Dashed\DashedEcommerceCore\Models\OrderLog;
+use Filament\Forms\Concerns\InteractsWithForms;
 use Dashed\DashedTranslations\Models\Translation;
 use Dashed\DashedEcommerceCore\Models\DiscountCode;
 use Dashed\DashedEcommerceCore\Models\OrderPayment;
 use Dashed\DashedEcommerceCore\Models\OrderProduct;
 use Dashed\DashedEcommerceCore\Models\ProductExtra;
 use Dashed\DashedEcommerceCore\Classes\ShoppingCart;
+use Dashed\DashedEcommerceCore\Models\PaymentMethod;
 use Dashed\DashedEcommerceCore\Classes\CurrencyHelper;
 use Dashed\DashedEcommerceCore\Models\ProductExtraOption;
 
@@ -235,12 +234,12 @@ trait CreateManualOrderActions
             }
         }
 
-        if (!$this->discount_code) {
+        if (! $this->discount_code) {
             session(['discountCode' => '']);
             $this->activeDiscountCode = null;
         } else {
             $discountCode = DiscountCode::usable()->where('code', $this->discount_code)->first();
-            if (!$discountCode || !$discountCode->isValidForCart()) {
+            if (! $discountCode || ! $discountCode->isValidForCart()) {
                 session(['discountCode' => '']);
                 $this->activeDiscountCode = null;
             } else {
@@ -267,7 +266,7 @@ trait CreateManualOrderActions
             }
         }
 
-        if (!$shippingMethod) {
+        if (! $shippingMethod) {
             $this->shipping_method_id = null;
         }
 
@@ -303,7 +302,7 @@ trait CreateManualOrderActions
         $cartItems = ShoppingCart::cartItems($this->cartInstance);
         $checkoutData = ShoppingCart::getCheckoutData($this->shipping_method_id, $this->payment_method_id);
 
-        if (!$cartItems) {
+        if (! $cartItems) {
             Notification::make()
                 ->title(Translation::get('no-items-in-cart', 'cart', 'You dont have any products in your shopping cart'))
                 ->danger()
@@ -339,7 +338,7 @@ trait CreateManualOrderActions
             }
         }
 
-        if (!$shippingMethod && $this->orderOrigin != 'pos') {
+        if (! $shippingMethod && $this->orderOrigin != 'pos') {
             //            Notification::make()
             //                ->title('Ga een stap terug, klik op "Gegevens bijwerken" en ga door')
             //                ->danger()
@@ -356,10 +355,10 @@ trait CreateManualOrderActions
 
         $discountCode = DiscountCode::usable()->where('code', session('discountCode'))->first();
 
-        if (!$discountCode) {
+        if (! $discountCode) {
             session(['discountCode' => '']);
             $discountCode = '';
-        } elseif ($discountCode && !$discountCode->isValidForCart($this->email)) {
+        } elseif ($discountCode && ! $discountCode->isValidForCart($this->email)) {
             session(['discountCode' => '']);
 
             Notification::make()
@@ -604,7 +603,7 @@ trait CreateManualOrderActions
                 }
             }
 
-            if (!$productAlreadyInCart) {
+            if (! $productAlreadyInCart) {
                 $this->products[] = [
                     'id' => $selectedProduct['id'],
                     'product' => $selectedProduct,
@@ -698,7 +697,7 @@ trait CreateManualOrderActions
 
     public function toggleCustomProductPopup()
     {
-        $this->customProductPopup = !$this->customProductPopup;
+        $this->customProductPopup = ! $this->customProductPopup;
     }
 
     public function getForms(): array
@@ -795,7 +794,7 @@ trait CreateManualOrderActions
                     ->required(),
                 TextInput::make('note')
                     ->label('Reden voor korting')
-                    ->visible(fn(Get $get) => $get('type') != 'discountCode')
+                    ->visible(fn (Get $get) => $get('type') != 'discountCode')
                     ->reactive(),
                 TextInput::make('amount')
                     ->label('Prijs')
@@ -806,7 +805,7 @@ trait CreateManualOrderActions
                     ->required()
                     ->prefix('€')
                     ->reactive()
-                    ->visible(fn(Get $get) => $get('type') == 'amount')
+                    ->visible(fn (Get $get) => $get('type') == 'amount')
                     ->helperText('Bij opslaan wordt er een kortingscode gemaakt die 30 minuten geldig is.'),
                 TextInput::make('percentage')
                     ->label('Percentage')
@@ -818,7 +817,7 @@ trait CreateManualOrderActions
                     ->default(21)
                     ->prefix('%')
                     ->reactive()
-                    ->visible(fn(Get $get) => $get('type') == 'percentage')
+                    ->visible(fn (Get $get) => $get('type') == 'percentage')
                     ->helperText('Bij opslaan wordt er een kortingscode gemaakt die 30 minuten geldig is.'),
                 Select::make('discountCode')
                     ->label('Kortings code')
@@ -834,7 +833,7 @@ trait CreateManualOrderActions
                         return $options;
                     })
                     ->required()
-                    ->visible(fn(Get $get) => $get('type') == 'discountCode'),
+                    ->visible(fn (Get $get) => $get('type') == 'discountCode'),
 
             ])
             ->statePath('createDiscountData');
@@ -842,12 +841,13 @@ trait CreateManualOrderActions
 
     public function submitCreateDiscountForm()
     {
-        if (!$this->products) {
+        if (! $this->products) {
             Notification::make()
                 ->title('Geen producten in winkelmand')
                 ->danger()
                 ->send();
             $this->createDiscountPopup = false;
+
             return;
         }
 
@@ -873,7 +873,7 @@ trait CreateManualOrderActions
             $this->discount_code = $discountCode->code;
         }
 
-        if (!$discountCode) {
+        if (! $discountCode) {
             Notification::make()
                 ->title('Kortingscode niet gevonden')
                 ->danger()
@@ -887,7 +887,7 @@ trait CreateManualOrderActions
 
     public function toggleVariable($variable)
     {
-        $this->{$variable} = !$this->{$variable};
+        $this->{$variable} = ! $this->{$variable};
     }
 
     public function removeDiscount()
@@ -924,11 +924,12 @@ trait CreateManualOrderActions
 
     public function initiateCheckout()
     {
-        if (!$this->products) {
+        if (! $this->products) {
             Notification::make()
                 ->title('Geen producten in winkelmand')
                 ->danger()
                 ->send();
+
             return;
         }
 
@@ -984,13 +985,13 @@ trait CreateManualOrderActions
                     ->inputMode('decimal')
                     ->required()
                     ->extraInputAttributes([
-                        'class' => 'text-xl sm:text-xl md:text-xl py-2'
+                        'class' => 'text-xl sm:text-xl md:text-xl py-2',
                     ])
                     ->extraFieldWrapperAttributes([
-                        'class' => 'text-xl sm:text-xl md:text-xl py-2'
+                        'class' => 'text-xl sm:text-xl md:text-xl py-2',
                     ])
                     ->extraAttributes([
-                        'class' => 'text-xl sm:text-xl md:text-xl py-2'
+                        'class' => 'text-xl sm:text-xl md:text-xl py-2',
                     ])
                     ->prefix('€'),
             ]);
@@ -1014,17 +1015,19 @@ trait CreateManualOrderActions
 
     public function markAsPaid()
     {
-        if ($this->paymentMethod->is_cash_payment && !$this->cashPaymentAmount) {
+        if ($this->paymentMethod->is_cash_payment && ! $this->cashPaymentAmount) {
             Notification::make()
                 ->title('Geen bedrag ingevoerd')
                 ->danger()
                 ->send();
+
             return;
         } elseif ($this->paymentMethod->is_cash_payment && $this->cashPaymentAmount < $this->totalUnformatted) {
             Notification::make()
                 ->title('Bedrag is te laag')
                 ->danger()
                 ->send();
+
             return;
         }
 
