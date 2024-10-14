c381ce8dccd0d4cca8929f50c6812d11035a57a3
Robin van Maasakker <robin@qubiqx.com>
2024-10-14 22:10:43 +0200

    Update

--- resources/views/pos/pages/customer-point-of-sale.blade.php
+++ resources/views/pos/pages/customer-point-of-sale.blade.php
@@ -20,22 +20,24 @@
                                     </svg>
                                 </button>
                             @endif
-                            {{--                            <button id="exitFullscreenBtn"--}}
-                            {{--                                    class="@if(!$fullscreen) hidden @endif h-12 w-12 bg-primary-500 text-white hover:bg-primary-700 transition-all duration-300 ease-in-out p-1 rounded-full flex items-center justify-center">--}}
-                            {{--                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"--}}
-                            {{--                                     stroke-width="1.5" stroke="currentColor" class="size-6">--}}
-                            {{--                                    <path stroke-linecap="round" stroke-linejoin="round"--}}
-                            {{--                                          d="M9 9V4.5M9 9H4.5M9 9 3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5 5.25 5.25"/>--}}
-                            {{--                                </svg>--}}
-                            {{--                            </button>--}}
-                            {{--                            <button id="fullscreenBtn"--}}
-                            {{--                                    class="@if($fullscreen) hidden @endif h-12 w-12 bg-primary-500 text-white hover:bg-primary-700 transition-all duration-300 ease-in-out p-1 rounded-full flex items-center justify-center">--}}
-                            {{--                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"--}}
-                            {{--                                     stroke-width="1.5" stroke="currentColor" class="size-6">--}}
-                            {{--                                    <path stroke-linecap="round" stroke-linejoin="round"--}}
-                            {{--                                          d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"/>--}}
-                            {{--                                </svg>--}}
-                            {{--                            </button>--}}
+                                                        <button id="exitFullscreenBtn"
+                                                                class="@if(!$fullscreen) hidden @endif h-12 w-12 bg-primary-500 text-white hover:bg-primary-700 transition-all duration-300 ease-in-out p-1 rounded-full flex items-center justify-center">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                 viewBox="0 0 24 24"
+                                                                 stroke-width="1.5" stroke="currentColor"
+                                                                 class="size-6">
+                                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                                      d="M9 9V4.5M9 9H4.5M9 9 3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5 5.25 5.25"/>
+                                                            </svg>
+                                                        </button>
+                                <button id="fullscreenBtn"
+                                        class="@if($fullscreen) hidden @endif h-12 w-12 bg-primary-500 text-white hover:bg-primary-700 transition-all duration-300 ease-in-out p-1 rounded-full flex items-center justify-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"/>
+                                    </svg>
+                                </button>
                         </div>
                     </div>
                     {{--                <div class="p-4 rounded-lg border border-gray-400 grid gap-4">--}}
