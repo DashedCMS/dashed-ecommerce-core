4173ea414f8e19b55035d4a8006dcec768c747ea
Robin van Maasakker <robin@qubiqx.com>
2024-12-05 09:01:52 +0100

    Update

--- resources/component-templates/blocks/few-products.blade.php
+++ resources/component-templates/blocks/few-products.blade.php
@@ -1,5 +1,5 @@
-<div class="{{ $data['backgroundColor'] ?? 'bg-white' }} py-24 sm:py-32">
-    <x-container>
+<div class="{{ $data['backgroundColor'] ?? 'bg-white' }} @if($data['top_margin'] ?? true) pt-24 sm:pt-36 @endif @if($data['bottom_margin'] ?? true) pb-24 sm:pb-36 @endif">
+    <x-container :show="$data['in_container'] ?? true">
         <div class="mx-auto max-w-2xl lg:max-w-none">
             <div class="text-center"
                  data-aos="fade-up">
@@ -10,12 +10,21 @@
                     <p class="mt-4 text-lg/8 text-gray-600">{{ $data['subtitle'] }}</p>
                 @endif
             </div>
-            @php($products = ($products ?? false) ? $products : (\Dashed\DashedEcommerceCore\Classes\Products::getAllV2('4', orderBy: 'latest', enableFilters: false)['products'] ?? []))
+            @if($data['products'] ?? false)
+                @php($products = \Dashed\DashedEcommerceCore\Models\Product::publicShowable()->whereIn('id', $data['products'])->get())
+            @elseif($products ?? false)
+                @php($products = $products)
+            @elseif($data['useCartRelatedItems'] ?? false)
+                @php($products = ShoppingCart::getCrossSellAndSuggestedProducts($data['amount_of_products'] ?? 4))
+            @else
+                @php($products = Products::getAllV2($data['amount_of_products'] ?? 4, orderBy: 'latest', enableFilters: false)['products'] ?? [])
+            @endif
             @if(count($products))
-                <div class="grid gap-8 items-start md:grid-cols-2 lg:grid-cols-4 mt-4">
+                <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-4 mt-4">
                     @foreach($products as $product)
                         <div data-aos="fade-right" data-aos-delay="{{ 300 * $loop->iteration }}">
-                            <x-product.product :product="$product" :backgroundColor="$data['backgroundColor'] ?? 'bg-white'" />
+                            <x-product.product :product="$product"
+                                               :backgroundColor="$data['backgroundColor'] ?? 'bg-white'"/>
                         </div>
                     @endforeach
                 </div>
