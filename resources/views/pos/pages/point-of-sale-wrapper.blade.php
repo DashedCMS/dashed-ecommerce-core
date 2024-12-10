903afb6283c76d8cbd76137fd2666f669f0c6504
Robin van Maasakker <robin@qubiqx.com>
2024-12-10 20:03:55 +0100

    Update

--- resources/views/pos/pages/point-of-sale-wrapper.blade.php
+++ resources/views/pos/pages/point-of-sale-wrapper.blade.php
@@ -32,7 +32,7 @@
         }
     </style>
 
-    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
 </head>
 
 <body class="font-sans antialiased min-h-dvh bg-black text-white overflow-hidden">
