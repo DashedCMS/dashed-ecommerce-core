7279340418efa5b7a04fcc16b6b6ca58d0288616
Robin van Maasakker <robin@qubiqx.com>
2025-07-01 18:50:22 +0200

    Update

--- resources/views/components/frontend/body-extend.blade.php
+++ resources/views/components/frontend/body-extend.blade.php
@@ -1,14 +1,14 @@
 @if(env('APP_ENV') != 'local')
-    {{--    @if(isset($product))--}}
-    {{--        <x-dashed-ecommerce-core::frontend.products.schema--}}
-    {{--            :product="$product"></x-dashed-ecommerce-core::frontend.products.schema>--}}
-    {{--    @endif--}}
-    {{--    @if(isset($products))--}}
-    {{--        @foreach($products as $product)--}}
-    {{--            <x-dashed-ecommerce-core::frontend.products.schema--}}
-    {{--                :product="$product"></x-dashed-ecommerce-core::frontend.products.schema>--}}
-    {{--        @endforeach--}}
-    {{--    @endif--}}
+    @if(isset($product))
+        <x-dashed-ecommerce-core::frontend.products.schema
+            :product="$product"></x-dashed-ecommerce-core::frontend.products.schema>
+    @endif
+    @if(isset($products))
+        @foreach($products as $product)
+            <x-dashed-ecommerce-core::frontend.products.schema
+                :product="$product"></x-dashed-ecommerce-core::frontend.products.schema>
+        @endforeach
+    @endif
 
     @if(isset($order) && $order->isPaidFor())
         @if(Customsetting::get('facebook_pixel_conversion_id') || Customsetting::get('facebook_pixel_store_id'))
@@ -18,24 +18,24 @@
         @endif
     @endif
 
-{{--    @script--}}
-{{--    <script>--}}
-{{--        $wire.on('productAddedToCart', (event) => {--}}
-{{--            dataLayer.push({--}}
-{{--                'event': 'addToCart',--}}
-{{--                'ecommerce': {--}}
-{{--                    'currencyCode': 'EUR',--}}
-{{--                    'add': {--}}
-{{--                        'products': [{--}}
-{{--                            'name': event[0].productName,--}}
-{{--                            'id': event[0].product.id,--}}
-{{--                            'price': event[0].price,--}}
-{{--                            'quantity': event[0].quantity,--}}
-{{--                        }]--}}
-{{--                    }--}}
-{{--                }--}}
-{{--            });--}}
-{{--        });--}}
-{{--    </script>--}}
-{{--    @endscript--}}
+    <script>
+        document.addEventListener('livewire:init', () => {
+            Livewire.on('productAddedToCart', (event) => {
+                dataLayer.push({
+                    'event': 'addToCart',
+                    'ecommerce': {
+                        'currencyCode': 'EUR',
+                        'add': {
+                            'products': [{
+                                'name': event[0].productName,
+                                'id': event[0].product.id,
+                                'price': event[0].price,
+                                'quantity': event[0].quantity,
+                            }]
+                        }
+                    }
+                });
+            });
+        });
+    </script>
 @endif
