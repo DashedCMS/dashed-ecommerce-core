ee0e7cc0029258e12febc5a78198733841a2a958
Robin van Maasakker <robin@qubiqx.com>
2025-07-01 17:19:08 +0200

    Update

--- resources/views/components/frontend/body-extend.blade.php
+++ resources/views/components/frontend/body-extend.blade.php
@@ -1,14 +1,14 @@
 @if(env('APP_ENV') != 'local')
-    @if(isset($product))
-        <x-dashed-ecommerce-core::frontend.products.schema
-            :product="$product"></x-dashed-ecommerce-core::frontend.products.schema>
-    @endif
-    @if(isset($products))
-        @foreach($products as $product)
-            <x-dashed-ecommerce-core::frontend.products.schema
-                :product="$product"></x-dashed-ecommerce-core::frontend.products.schema>
-        @endforeach
-    @endif
+{{--    @if(isset($product))--}}
+{{--        <x-dashed-ecommerce-core::frontend.products.schema--}}
+{{--            :product="$product"></x-dashed-ecommerce-core::frontend.products.schema>--}}
+{{--    @endif--}}
+{{--    @if(isset($products))--}}
+{{--        @foreach($products as $product)--}}
+{{--            <x-dashed-ecommerce-core::frontend.products.schema--}}
+{{--                :product="$product"></x-dashed-ecommerce-core::frontend.products.schema>--}}
+{{--        @endforeach--}}
+{{--    @endif--}}
 
     @if(isset($order) && $order->isPaidFor())
         @if(Customsetting::get('facebook_pixel_conversion_id') || Customsetting::get('facebook_pixel_store_id'))
