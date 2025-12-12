9bf60ed3aece7064a2863d9da5f2357285943a49
Robin van Maasakker <robin@dashed.nl>
2025-12-12 09:44:54 +0100

    Update

--- src/Filament/Resources/OrderResource/Pages/ViewOrder.php
+++ src/Filament/Resources/OrderResource/Pages/ViewOrder.php
@@ -28,16 +28,16 @@ class ViewOrder extends ViewRecord
     {
         $invoiceUrl = $this->record->downloadInvoiceUrl();
         $packingSlipUrl = $this->record->downloadPackingslipUrl();
-        $previousOrder = Order::where('id', '<', $this->record->id)
+        $previousOrder = $this->record->fulfillment_status == 'unhandled' ? Order::where('id', '<', $this->record->id)
             ->orderBy('id', 'desc')
             ->where('fulfillment_status', 'unhandled')
             ->isPaid()
-            ->first();
-        $nextOrder = Order::where('id', '>', $this->record->id)
+            ->first() : '';
+        $nextOrder = $this->record->fulfillment_status == 'unhandled' ? Order::where('id', '>', $this->record->id)
             ->orderBy('id', 'asc')
             ->where('fulfillment_status', 'unhandled')
             ->isPaid()
-            ->first();
+            ->first() : '';
 
         return array_merge([
             Action::make('viewInWebsite')
@@ -45,20 +45,24 @@ class ViewOrder extends ViewRecord
                 ->icon('heroicon-s-globe-alt')
                 ->button()
                 ->url($this->record->getUrl())
+                ->tooltip('Bekijk bestelling in de webshop')
                 ->openUrlInNewTab(),
             Action::make('edit')
                 ->hiddenLabel()
                 ->icon('heroicon-s-pencil-square')
+                ->tooltip('Bewerk bestelling')
                 ->button()
                 ->url(route('filament.dashed.resources.orders.edit', ['record' => $this->record])),
             Action::make('Factuur')
                 ->button()
+                ->tooltip('Download de factuur als PDF')
                 ->icon('heroicon-s-arrow-down-tray')
                 ->url($invoiceUrl)
                 ->visible((bool)$invoiceUrl),
             Action::make('Pakbon')
                 ->button()
                 ->icon('heroicon-s-arrow-down-tray')
+                ->tooltip('Download de pakbon als PDF')
                 ->url($packingSlipUrl)
                 ->visible((bool)$packingSlipUrl),
             Action::make('Vorige bestelling')
@@ -66,13 +70,15 @@ class ViewOrder extends ViewRecord
                 ->hiddenLabel()
                 ->icon('heroicon-s-arrow-left')
                 ->url(fn() => $previousOrder ? route('filament.dashed.resources.orders.view', ['record' => $previousOrder->id]) : '')
-                ->visible((bool)$previousOrder),
+                ->visible((bool)$previousOrder)
+                ->tooltip('Bekijk de vorige onverwerkte bestelling'),
             Action::make('Volgende bestelling')
                 ->button()
                 ->hiddenLabel()
                 ->icon('heroicon-s-arrow-right')
                 ->url(fn() => $nextOrder ? route('filament.dashed.resources.orders.view', ['record' => $nextOrder->id]) : '')
-                ->visible((bool)$nextOrder),
+                ->visible((bool)$nextOrder)
+                ->tooltip('Bekijk de volgende onverwerkte bestelling'),
         ], ecommerce()->buttonActions('order'));
     }
 
